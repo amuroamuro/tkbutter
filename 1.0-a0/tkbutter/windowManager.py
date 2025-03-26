968f1d2649dfc1c0943cdbc3778ed159acbf2b9e
trycus <mbhstar1206@gmail.com>
2025-03-26 19:32:23 +0900

    됐다

--- 1.0-a0/tkbutter/windowManager.py
+++ 1.0-a0/tkbutter/windowManager.py
@@ -11,7 +11,7 @@ class WindowManager:
         self.__window.resizable(width=resizing_x, height=resizing_y)
         self.__wizet_list = []
         self.__frame_list = [self.__window]
-        self.__font_list = []
+        self.__font_list = [Font()]
 
     def get_window():
         return 0
@@ -21,9 +21,9 @@ class WindowManager:
         self.__font_list.append(Font(family=family, size=size, weight=weight, slant=slant, underline=underline, overstrike=overstrike))
 
     #위젯 매니저
-    def create_label(self, frame_number: int=0, text: str="Do you want some BUTTER?", font=Font()):
+    def create_label(self, frame_number: int=0, text: str="Do you want some BUTTER?", font: int=0):
         wizet_number = len(self.__wizet_list)
-        self.__wizet_list.append(Label(self.__frame_list[frame_number], text=text, font=font))
+        self.__wizet_list.append(Label(self.__frame_list[frame_number], text=text, font=self.__font_list[font]))
 
     def set_size(self, geometry: str):
         self.__window.geometry(geometry)
