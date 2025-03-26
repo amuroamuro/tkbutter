50971c114347e9fee9cca883b017896643f948c8
trycus <mbhstar1206@gmail.com>
2025-03-26 19:22:21 +0900

    a0완성

---
+++ 1.0-a0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    title="tkbutter",
+    version="1.0-a0",
+    description="tkinter의 복잡한 셋팅을 간소화하는 starWalker의 확장패키지임 and, Do you want some butter?",
+    packages=["tkbutter"]
+)
---
+++ 1.0-a0/tkbutter/windowManager.py
@@ -0,0 +1,32 @@
+
+from tkinter import *
+from tkinter import font
+from tkinter.font import *
+
+#윈도우를 만들 때 복잡한 과정들을 메소드화하는 클래스를 만든다. 그게 windowManager
+class WindowManager:
+    def __init__(self, title: str="Do you want some BUTTER?", resizing_x: bool=True, resizing_y: bool=True):
+        self.__window = Tk()
+        self.__window.title(title)
+        self.__window.resizable(width=resizing_x, height=resizing_y)
+        self.__wizet_list = []
+        self.__frame_list = [self.__window]
+        self.__font_list = []
+
+    def get_window():
+        return 0
+
+    def create_font(self, family: str="TkDefaultFont", size: int=16, weight: str="mormal", slant: str="roman", underline: bool=False, overstrike: bool=False):
+        font_number = len(self.__font_list)
+        self.__font_list.append(Font(family=family, size=size, weight=weight, slant=slant, underline=underline, overstrike=overstrike))
+
+    #위젯 매니저
+    def create_label(self, frame_number: int=0, text: str="Do you want some BUTTER?", font=Font()):
+        wizet_number = len(self.__wizet_list)
+        self.__wizet_list.append(Label(self.__frame_list[frame_number], text=text, font=font))
+
+    def set_size(self, geometry: str):
+        self.__window.geometry(geometry)
+
+    def main_loop_window(self):
+        self.__window.mainloop()
