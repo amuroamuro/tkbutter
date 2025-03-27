37b48ecf3bc7e608c21ae9f8fd365b132b1b2ba4
trycus <mbhstar1206@gmail.com>
2025-03-27 17:44:18 +0900

    이제 1.0a1버전 완성!!

--- 1.0-a1/tkbutter/windowManager.py
+++ 1.0-a1/tkbutter/windowManager.py
@@ -2,6 +2,7 @@
 from ast import Lambda
 from tkinter import *
 from tkinter import font
+import tkinter
 from tkinter.font import *
 from typing import Any, Callable
 
@@ -11,7 +12,7 @@ class WindowManager:
         self.__window = Tk()
         self.__window.title(title)
         self.__window.resizable(width=resizing_x, height=resizing_y)
-        self.__wizet_list = []
+        self.__widget_list: list[tkinter.Widget]= []
         self.__frame_list = [self.__window]
         self.__font_list = [Font()]
 
@@ -31,13 +32,23 @@ class WindowManager:
 
     #위젯 매니저
     def create_label(self, frame_number: int=0, text: str="Do you want some BUTTER?", font_number: int=0):
-        wizet_number = len(self.__wizet_list)
-        self.__wizet_list.append(Label(self.__frame_list[frame_number], text=text, font=self.__font_list[font_number]))
+        wizet_number = len(self.__widget_list)
+        self.__widget_list.append(Label(self.__frame_list[frame_number], text=text, font=self.__font_list[font_number]))
         return wizet_number
 
-    def create_button(self, frame_number, text: str="Do you want some BUTTER?", commend: Callable[..., Any]=print("Do you want some BUTTER?"), font_number: int=0):
-        wizet_number = len(self.__wizet_list)
-        self.__wizet_list.append(Button(self.__frame_list[frame_number], text=text, command=commend, font=self.__font_list[font_number]))
+    def create_button(self, frame_number, text: str="Do you want some BUTTER?", commend: Callable[..., Any]=print("Do you want some BUTTER?"), font_number: int=0, width: int=0, hight: int=0):
+        wizet_number = len(self.__widget_list)
+        self.__widget_list.append(Button(self.__frame_list[frame_number], text=text, command=commend, font=self.__font_list[font_number], width=width, hight=hight))
+        return wizet_number
+
+
+
+    #pack and grid
+    def pack_widget(self, wizet: int=0, side: str="Center", padx: int=0, pady: int=0):
+        self.__widget_list[wizet].pack(side=side, padx=padx, pady=pady)
+
+    def grid_widget(self, widget: int=0, row: int=0, column: int=0, rowspan: int=1, columnspan: int=1, padx: int=0, pady: int=0):
+        self.__widget_list[widget].grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan, padx=padx, pady=pady)
 
 
 
