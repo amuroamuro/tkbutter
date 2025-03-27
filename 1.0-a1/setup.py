4bd6ba570895b1962fb5e26c8c2046ac662b1902
trycus <mbhstar1206@gmail.com>
2025-03-27 17:17:11 +0900

    아아..

---
+++ 1.0-a1/setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    title="tkbutter",
+    version="1.0a1",
+    description="이건 tkinter의 복잡한 셋팅을 깔끔(smooth)하게 만드는 tkinter확장모듈의 알파버전입니다.",
+    packages=["tkbutter"]
+)
